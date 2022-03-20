081e827154f1ab9b02ad78ac6ffa3199c0788e28
cianmawhinney <cianmawhinney@gmail.com>
2022-03-20 00:17:21 +0000

    Deploying to gh-pages from @ cianmawhinney/microsoft-pneumothorax-classifier@fca2abab4ab03576f183620bbed926722397758f 🚀

--- uploading.js
+++ uploading.js
@@ -1,17 +1,27 @@
-const data = new FormData();
-const images = document.querySelector('input[type="file"][multiple]');
-for (let i = 0; i < images.files.length; i++) {
-    data.append('img_${i}', images.files[i]);
-}
+async function uploadImage() {
+  const endpointURL = "https://pneumothorax.mawh.in/score"
+  const apiKey = "LDzXDtJKHklAX2uhlDHRdbf5DRcTgXYf";
 
-fetch('https://wherever.it.goes/newData', {
+  const data = new FormData();
+  const images = document.getElementById('customFile');
+  // const images = document.querySelector('input[type="file"][multiple]');
+  for (let i = 0; i < images.files.length; i++) {
+    data.append('img_${i}', images.files[i]);
+  }
+  
+  await fetch(endpointURL, {
     method: 'POST',
     body: data,
-})
-.then(response => response.json())
-.then(result => {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${apiKey}`,
+    },
+  })
+  .then(response => response.json())
+  .then(result => {
     console.log('Success:', result);
-})
-.catch(error => {
+  })
+  .catch(error => {
     console.error('Error:', error);
-});
+  });
+}
