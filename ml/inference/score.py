b5b83a15d5ccf6e25eac8ce5a607f0a07e7a06d6
Cian Mawhinney <cianmawhinney@gmail.com>
2022-03-21 09:30:05 +0000

    Update comments in score.py

--- ml/inference/score.py
+++ ml/inference/score.py
@@ -17,6 +17,7 @@ def init():
     # AZUREML_MODEL_DIR points to the folder ./azureml-models/$MODEL_NAME/$VERSION
     model_folder = os.path.join(os.getenv("AZUREML_MODEL_DIR"), "outputs")
     model = tf.keras.models.load_model(model_folder)
+    print('Model loaded')
     print(model)
 
 @rawhttp
@@ -24,25 +25,19 @@ def run(request):
     print(request)
 
     if request.method == 'POST':
+        # grab the image from the request
         file_bytes = request.files["image"]
         image = Image.open(file_bytes).convert('RGB')
 
+        # TODO: Build this step into the model itself
+        # convert the image into the right sized numpy array for the model to work with
         new_image_size = (512, 512)
         image = image.resize(new_image_size)
         image_data = tf.keras.preprocessing.image.img_to_array(image)
-        
         image_data = image_data.reshape(1, 512, 512, 3)
 
+        # return the predictions based on the model
         predictions = model.predict(image_data).tolist()
         return AMLResponse(json.dumps(predictions), 200)
     else:
         return AMLResponse("Bad Request", 500)
-    
-
-    # need to work out the format of the data coming into the function (could be in PIL format or a
-    # raw PNG maybe?)
-    # ... if that decision is up to us - probably just accept a PNG image, then convert it
-    # then transform the image into the format the model wants
-    # Input is a 3D array with dimensions n,n,3 (1 channel each for RGB and image width n)
-    # tf.keras.layers.InputLayer(input_shape=IMAGE_SIZE + (3,)),
-    # then I think we can just do model.predict(image) and return the results????
