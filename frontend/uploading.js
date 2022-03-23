70c0964d643eaaed5c944daacbe3dac379c5f549
KennethHarmon <kennethrharmon@gmail.com>
2022-03-23 21:46:40 +0000

    bootstrapified the layout, added transition

--- frontend/uploading.js
+++ frontend/uploading.js
@@ -1,4 +1,5 @@
 async function uploadImage() {
+  submittedUI();
   const endpointURL = "https://pneumothorax.mawh.in/score"
   const apiKey = "LDzXDtJKHklAX2uhlDHRdbf5DRcTgXYf";
 
@@ -23,4 +24,9 @@ async function uploadImage() {
   .catch(error => {
     console.error('Error:', error);
   });
+}
+
+function submittedUI() {
+  $(".main-content").hide()
+  $(".image-results").css("display", "flex")
 }
