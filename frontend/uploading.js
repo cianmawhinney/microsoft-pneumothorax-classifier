abb351952f02bfddddfdc6630030b973ff65bd31
elliot-lyons <elliot243lyons@gmail.com>
2022-03-25 20:01:58 +0000

    Added home, previous and next buttons into index.html.
    Deleted results.html
    
    Added functions into uploading.js to deal with added buttons.

--- frontend/uploading.js
+++ frontend/uploading.js
@@ -29,4 +29,60 @@ async function uploadImage() {
 function submittedUI() {
   $(".main-content").hide()
   $(".image-results").css("display", "flex")
+
+  // if (imageArray.length < 2)                     // if there's not a batch of images, no need to display prev and next buttons
+  // {
+  //   $(".prev-button").hide();
+  //   $(".next-button").hide();
+  // }
 }
+
+function goHome()             // add all these into 'app.js'?
+{
+  $(".image-results").hide();
+  $(".main-content").css("display", "flex");
+};
+
+function goPrev()
+{
+  // if (imageIndex > 0)           // should be somewhat like this?
+  // {
+  //   imageIndex--;
+  //   display image at index imageIndex;
+  // }
+
+  // if (imageIndex == 0)         // if there's no need for a prev button
+  // {
+    $(".prev-button").hide();
+  //   prevHidden = true;
+  // }       
+
+
+  // if (nextHidden && imageArray.length > 1)              // if next is hidden and we will now need it again after going back one image
+  // {
+  //   $(".next-button").css("display", "flex");
+  //   nextHidden = false;
+  // }
+};
+
+function goNext()                 // functions are more or less the same as in goPrev, just flipped to cater for the opposite as such
+{
+  // if (imageIndex > imageArray.length)          
+  // {
+  //   imageIndex++;
+  //   display image at index imageIndex;
+  // }
+
+  // if (imageIndex == (imageArray.length - 1))
+  // {
+    $(".next-button").hide();
+  //   nextHidden = true;
+  // }       
+
+
+  // if (prevHidden && imageArray.length > 1)
+  // {
+  //   $(".prev-button").css("display", "flex");
+  //   prevHidden = false;
+  // }
+};
